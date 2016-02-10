4c0a14dec08faafa2eb15e1f7b9892dbd4fd1e75
Vladimir Acimovic <pupin@vladimirs-macbook-pro.local>
2016-02-10 09:46:16 +0100

    Initial Commit

---
+++ hubTests/hubTests.swift
@@ -0,0 +1,36 @@
+//
+//  hubTests.swift
+//  hubTests
+//
+//  Created by Vladimir Acimovic on 2/10/16.
+//  Copyright © 2016 Srpsko blago. All rights reserved.
+//
+
+import XCTest
+@testable import hub
+
+class hubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
